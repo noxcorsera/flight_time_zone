bbb46b07fded07e39b6c546fbec8ed705d349d57
Philip Lenox <philenox@gmail.com>
2024-12-24 06:18:43 +0000

    basic plotting working, too high to complete

--- flight_path_extractor.py
+++ flight_path_extractor.py
@@ -3,9 +3,15 @@ from datetime import datetime
 from pprint import pp
 from dateutil import tz
 from suntime import Sun, SunTimeException
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+DEBUG = True
 
 class FlightPath:
     def __init__(self, path, date):
+        if DEBUG:
+            print("Initialize")
         self.path = path
         self.date = date
         self.read_excel_to_dataframe()
@@ -19,10 +25,58 @@ class FlightPath:
             datetime_string = f"{self.date} {time_string.split(' ')[1]}"
             am_pm_string = time_string.split(' ')[2]
             datetime_string = f"{datetime_string} {am_pm_string}"
-            time = datetime.strptime(datetime_string, '%Y-%m-%d %I:%M:%S %p').timestamp()
+            time = datetime.strptime(datetime_string, '%Y-%m-%d %I:%M:%S %p')
             lat = row['Latitude']
             long = row['Longitude']
             self.flight_path.append((time, lat, long))
+    
+    def calculate_sunset(self):
+        """
+        Calculates the sunset time at a given latitude and longitude.
+        
+        :param lat: Latitude of the location.
+        :param long: Longitude of the location.
+        :return: Sunset time at the given location.
+        """
+        if DEBUG:
+            print("Calculating Sunset")
+        self.sunset_time = []
+        for time, lat, long in self.flight_path:
+            sun = Sun(lat, long)
+            try:
+                sunset_time = sun.get_sunset_time()
+                self.sunset_time.append((time, sunset_time))
+            except SunTimeException as e:
+                print("Sun does not set at this location")
+            
+
+    # plot sunset times
+    def plot_sunset_times(self):
+        if DEBUG:
+            print("Plotting Sunset Times")
+        
+        fig, ax = plt.subplots()
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+        time = [x[0] for x in self.sunset_time]
+        sunset = [x[1] for x in self.sunset_time]
+        plt.plot(time, sunset)
+        plt.xlabel('Time')
+        plt.ylabel('Sunset Time')
+        plt.title('Sunset Time vs Time')
+        plt.show()
+
+
+    def calculate_time_to_sunset(self, time, lat, long):
+        """
+        Calculates the time until sunset at a given latitude and longitude.
+        
+        :param time: Current time.
+        :param lat: Latitude of the location.
+        :param long: Longitude of the location.
+        :return: Time until sunset at the given location.
+        """
+        sunset = self.calculate_sunset(lat, long)
+        return sunset - time
 
     def get_flight_path(self):
         return self.flight_path
@@ -41,5 +95,7 @@ if __name__ == "__main__":
     date = "2023-12-19"  # Example date
 
     flight = FlightPath(path, date)
+    flight.calculate_sunset()
     # pretty print flight path
-    pp(flight.get_flight_path())
+    # pp(flight.sunset_time)
+    flight.plot_sunset_times()
